a9b8eac203b4a95155f93cfd8785b9fcd01e613e
Brian Perry <contact.brianperry@gmail.com>
2017-04-11 21:11:36 -0500

    2: enabled Fractal Twig adapter

--- fractal/fractal.js
+++ fractal/fractal.js
@@ -29,3 +29,15 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  * Tell the Fractal web preview plugin where to look for static assets.
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+fractal.components.engine('@frctl/twig'); // use Twig for components
+fractal.components.set('ext', '.twig'); // use .twig file extension
+
+const mandelbrot = require('@frctl/mandelbrot');
+
+// Customize theme configuration
+const myCustomisedTheme = mandelbrot({
+  "nav": ["docs", "components"] // show docs above components in the sidebar
+});
+
+fractal.web.theme(myCustomisedTheme);
